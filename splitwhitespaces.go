b902e7aea82fd3961fa6b8a5345276d3d0cec1e5
Adsouza <anthonydsouza58@gmail.com>
2021-10-13 15:01:41 +0100

    new

---
+++ splitwhitespaces.go
@@ -0,0 +1,27 @@
+package tool
+
+func SplitWhiteSpaces(s string) []string {
+	var str []string
+
+	aRune := []rune(s)
+	l := len(aRune) - 1
+
+	var word string
+
+	for i, v := range s {
+		if i == l {
+			word = word + string(v)
+			str = append(str, word)
+		} else if v == 32 || v == 15 || v == 10 {
+			if aRune[i+1] == 32 || aRune[i+1] == 15 || aRune[i+1] == 10 {
+			} else {
+				str = append(str, word)
+				word = ""
+			}
+		} else {
+			word = word + string(v)
+		}
+	}
+
+	return str
+}
--- texttool/main.go
+++ texttool/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+	"tool"
 )
 
-func check(e error) {
+func Check(e error) {
 	if e != nil {
 		panic(e)
 	}
@@ -14,70 +16,40 @@ func check(e error) {
 
 func main() {
 	//// read sample.txt file
-	dat, err := os.ReadFile(os.Args[1])
+	input := os.Args[1]
+	dat, err := os.ReadFile(input)
+	Check(err)
+	/// split by whitespace
+	words := tool.SplitWhiteSpaces(string(dat))
 
-	check(err)
-
-	input := string(dat)
-	l := len([]rune(input)) - 1
-	aRune := []rune(input)
-
-	fmt.Println(input)
-
-	var output string
-
-	var str string
-
-	var op string
-
-	for i, v := range input {
-		/// spaces
-		if i == l { /// final
-			str = str + string(v)
-			output = output + str
-		} else if v == 40 {
-			// find op
-			for j := i + 1; aRune[j] != 41; j++ {
-				op = op + string(aRune[j])
-			}
-			fmt.Println(op)
-			/// apply op to str then add to output
-			if op == "hex" {
-				fmt.Println(str)
-				/// find last word
-				var words []string
-				var word string
-				for _, v := range str {
-					if v != ' ' {
-						word = word + string(v)
-					} else if v == ' ' {
-						words = append(words, word)
-						word = ""
-					}
-				}
-				lastindex := len(words) - 1
-
-				lastword := words[lastindex]
-
-				fmt.Println(lastword)
-
-				decimal, err := strconv.ParseInt(lastword, 16, 32)
-
-				dec := strconv.FormatInt(decimal, 10)
-
-				check(err)
-
-				for _, v := range words[:lastindex] {
-					output = output + v + " "
-				}
-				output = output + dec
-
-				fmt.Println(output)
-
-			}
-			op = ""
-		} else {
-			str = str + string(v)
+	for i, word := range words {
+		/// changes for hex operation
+		if word == "(hex)" {
+			hexa, err := strconv.ParseInt(words[i-1], 16, 64)
+			Check(err)
+			words[i-1] = strconv.FormatInt(hexa, 10)
+		}
+		/// changes for bin operation
+		if word == "(bin)" {
+			bin, err := strconv.ParseInt(words[i-1], 2, 64)
+			Check(err)
+			words[i-1] = strconv.FormatInt(bin, 10)
+		}
+		/// changes for cap operation
+		if word == "(cap)" {
+			words[i-1] = strings.Title(words[i-1])
 		}
 	}
+	fmt.Println(words)
+
+	/// remove operation tags into new variable
+	words1 := ""
+	for _, word := range words {
+		if word != "(hex)" && word != "(bin)" {
+			words1 = words1 + " " + word
+		}
+	}
+
+	fmt.Println(words1)
+
 }
