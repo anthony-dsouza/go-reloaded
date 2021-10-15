cd8e642cb27bb73993c358a85006fd96830ee34a
Adsouza <anthonydsouza58@gmail.com>
2021-10-15 12:11:56 +0100

    operations

--- texttool/main.go
+++ texttool/main.go
@@ -22,6 +22,8 @@ func main() {
 	/// split by whitespace
 	words := tool.SplitWhiteSpaces(string(dat))
 
+	fmt.Println(words)
+
 	for i, word := range words {
 		/// changes for hex operation
 		if word == "(hex)" {
@@ -39,14 +41,67 @@ func main() {
 		if word == "(cap)" {
 			words[i-1] = strings.Title(words[i-1])
 		}
+		/// changes for cap with respect to number
+		if word == "(cap," {
+			// finding the index of the last element
+			len := len(words[i+1]) - 1
+			// the value of the next word without the parenthesis
+			numb := words[i+1][:len]
+			// converting numb from a string to an integer
+			num, _ := strconv.Atoi(numb)
+			// capitalizing the words before the operation tag according to the number
+			for j := 1; j <= num; j++ {
+				words[i-j] = strings.Title(words[i-j])
+			}
+		}
+		/// changes for low with respect to number
+		if word == "(low," {
+			// finding the index of the last element
+			len := len(words[i+1]) - 1
+			// the value of the next word without the parenthesis
+			numb := words[i+1][:len]
+			// converting numb from a string to an integer
+			num, _ := strconv.Atoi(numb)
+			// lowercase the words before the operation tag according to the number
+			for j := 1; j <= num; j++ {
+				words[i-j] = strings.ToLower(words[i-j])
+			}
+		}
+		/// changes for up with respect to number
+		if word == "(up," {
+			// finding the index of the last element
+			len := len(words[i+1]) - 1
+			// the value of the next word without the parenthesis
+			numb := words[i+1][:len]
+			// converting numb from a string to an integer
+			num, _ := strconv.Atoi(numb)
+			// uppercase the words before the operation tag according to the number
+			for j := 1; j <= num; j++ {
+				words[i-j] = strings.ToUpper(words[i-j])
+			}
+		}
+		/// changes for up operation
+		if word == "(up)" {
+			words[i-1] = strings.ToUpper(words[i-1])
+		}
+		/// changes for low operation
+		if word == "(low)" {
+			words[i-1] = strings.ToLower(words[i-1])
+		}
 	}
-	fmt.Println(words)
 
-	/// remove operation tags into new variable
+	/// remove operation tags by only adding elements that are not equal to them to a new string
 	words1 := ""
-	for _, word := range words {
-		if word != "(hex)" && word != "(bin)" {
-			words1 = words1 + " " + word
+	for i, word := range words {
+		if 
+		if word != "(hex)" && word != "(bin)" && word != "(cap)" && word != "(up)" && word != "(low)" {
+			// add first word without space
+			if i == 0 {
+				words1 = words1 + word
+			} else {
+				words1 = words1 + " " + word
+			}
+
 		}
 	}
 
